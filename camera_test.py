6860742510e4d296d00dd9ffa63fa1ae6e68b451
Nicholas Hernandez <nicholashernandez39@gmail.com>
2018-01-27 09:27:20 -0500

    Making sure PiCamera works

---
+++ camera_test.py
@@ -0,0 +1,28 @@
+import picamera
+import io
+import os
+
+from time import sleep
+
+from serial import serial
+
+
+ser = Serial('/dev/ttyACM0', 9600)
+
+
+def describe_img(image_url):
+
+    file_name = os.path.join(
+        os.path.dirname(__file__),
+        image_url)
+
+        with io.open(file_name, 'rb') as image_file:
+            content = image_file.read()
+
+camera = picamera.PiCamera()
+counter = 0
+while True:
+    print 'Capturing image:', counter
+    camera.capture('image1.jpg')
+    describe_img('image1.jpg')
+    counter += 1
